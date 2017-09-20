0e5e93da381053e01bd9ae0dfd1b767206788864
Jonathan Lin <jonathan.fs.lin@gmail.com>
2017-09-20 11:49:53 -0400

    updated mvnsynctext m file

--- synchro4modalities_worstCase/mvnsandalsynctest.m
+++ synchro4modalities_worstCase/mvnsandalsynctest.m
@@ -8,34 +8,34 @@ addpath(genpath('data'));
 
 
 %% filepaths
-filepath_mocap = 'data/2017-09-14_TimeSyncTest/mocap/timesync1.trc';
-filepath_fp    = 'data/2017-09-14_TimeSyncTest/mocap/timesync1.anc';
+filepath_mocap = '../data/mocap_sept15/set1_smoothed.trc';
+filepath_fp    = '../data/mocap_sept15/set1.anc';
 
-filepath_mvn        = 'data/2017-09-14_TimeSyncTest/xsens/test_claudia-001.mvnx';
-filepath_san_left   = 'data/2017-09-14_TimeSyncTest/sandals/trial_left_001/data.log';
-filepath_san_right  = 'data/2017-09-14_TimeSyncTest/sandals/trial_right_001/data.log';
+filepath_mvn        = '../data/mvnx_sept15/mvnx/test_claudia-001.mvnx';
+filepath_san_left   = '../data/sandals_sept15/trial_left_00001/data.log';
+filepath_san_right  = '../data/sandals_sept15/trial_right_00001/data.log';
 
 %% extracting data
 
 % --------- xsens
 
-if exist ('theStruct.mat')
-     load ('theStruct.mat');
-else  
-    theStruct = xml_read(filepath_mvn);
-    save ('theStruct.mat');
-end
+% if exist ('data.mat')
+%      load ('data.mat');
+% else  
+    data.mvn = xml_read(filepath_mvn);
+%     save ('data.mat');
+% end
 
 %TODO
 % frameInfo = theStruct(2).Children.Children;
-% mvnTime = [];
-% mvnPos = [];
-% for i = 1:length(frameInfo)
-%     currTime = str2num(frameInfo(i).Attributes(2).Value) / 1000;
-%     currPos = sscanf(frameInfo(i).Children(9).Children.Data, '%f %f %f')';
-%     mvnTime = [mvnTime; currTime];
-%     mvnPos =  [mvnPos;  currPos];
-% end
+mvnTime = [];
+mvnPos = [];
+for i = 4:length(data.mvn.subject.frames.frame)
+    currTime = data.mvn.subject.frames.frame(i).ATTRIBUTE.ms / 1000;
+    currPos = data.mvn.subject.frames.frame(i).position;
+    mvnTime = [mvnTime; currTime];
+    mvnPos =  [mvnPos;  currPos];
+end
 
 % --------- left sandal
 data.sandals.left = dlmread(filepath_san_left , ' ');
@@ -48,17 +48,28 @@ sandalTime.right   = data.sandals.right(:, 2);
 sandalData.right   = data.sandals.right(:, 3);
 
 % --------- mocap 
+scanStr = '%f%f';
+for i = 1:80
+    scanStr = [scanStr '%f%f%f'];
+end
+
 fid = fopen(filepath_mocap);
-data.mocap = textscan(fid,'%f %f %f %f %f %f %f %f %f %f %f','HeaderLines',6);
+data.mocap = textscan(fid,scanStr,'HeaderLines',6,'Delimiter','/t');
 fclose(fid);
 mocapTime  = data.mocap{2};
-mocapData  = data.mocap{3};
+mocapData  = [data.mocap{3:end}];
 
 % --------- forceplates 
+scanStr = '%f';
+for i = 1:2
+    scanStr = [scanStr '%f%f%f%f%f%f%f%f'];
+end
+
 fid = fopen(filepath_fp);
-data.fp = textscan(fid,'%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f','HeaderLines',11);
+data.fp = textscan(fid,scanStr,'HeaderLines',11);
 fclose(fid);
 fpTime = data.fp{1};
+fpData = [data.fp{2:17}];
 
 %% aligning signals
 
@@ -71,11 +82,20 @@ fpTime = data.fp{1};
 
 %% plotting data
 
-% plot
-% figure;
-% h1 = subplot(211);
-% plot(mvnTime, mvnPos, '.');
-% h2 = subplot(212);
-% plot(sandalTime, sandalData, '.');
-% 
-% linkaxes([h1 h2], 'x');
+startTime = mvnTime(1);
+
+figure;
+h1 = subplot(221);
+plot(mvnTime - startTime, mvnPos, '.');
+title('mvn');
+h2 = subplot(222);
+plot(sandalTime.right - startTime, sandalData.right, '.'); hold on
+plot(sandalTime.left - startTime, sandalData.left, '.');
+title('sandal right+left');
+h3 = subplot(223);
+plot(mocapTime, mocapData, '.');
+title('mocap');
+h4 = subplot(224);
+plot(fpTime, fpData, '.');
+title('fp');
+linkaxes([h1 h2 h3 h4], 'x');
